b54bb64b55e36a778e28fa56cfd4b6d8fc9121ca
Jinho622 <aceho21@gmail.com>
2025-01-08 11:08:35 +0900

    feat: #1 Spring Boot 프로젝트 설정

---
+++ src/main/java/TtattaBackend/ttatta/TtattaApplication.java
@@ -0,0 +1,13 @@
+package TtattaBackend.ttatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtattaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtattaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TtattaBackend/ttatta/TtattaApplicationTests.java
@@ -0,0 +1,13 @@
+package TtattaBackend.ttatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtattaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
