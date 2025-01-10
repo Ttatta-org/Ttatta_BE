5e7946005d8cdd96a3d82b64845fb051d6a6e24f
BAEK0111 <baekjw111@gmail.com>
2025-01-11 00:52:20 +0900

    ### 제목 - (영문기준) 50자 이내 # feat: #9 카테고리 생성 api 구현
    ### 본문 - (영문 기준) 1줄 72자 이내
    # Controller에 @Operation 어노테이션을 사용하여 api 설명 작성

--- src/main/java/TtattaBackend/ttatta/web/controller/DiaryCategoryController.java
+++ src/main/java/TtattaBackend/ttatta/web/controller/DiaryCategoryController.java
@@ -6,6 +6,7 @@ import TtattaBackend.ttatta.domain.DiaryCategories;
 import TtattaBackend.ttatta.service.DiaryCategoryService.DiaryCategoryCommandService;
 import TtattaBackend.ttatta.web.dto.DiaryCategoryRequestDTO;
 import TtattaBackend.ttatta.web.dto.DiaryCategoryResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/categories")
 public class DiaryCategoryController {
     private final DiaryCategoryCommandService diaryCategoryCommandService;
+
+    @Operation(summary = "카테고리 생성 api", description =
+            "새로운 카테고리를 생성할 때 사용하는 api 입니다.\n카테고리 이름과 색상, 사용자의 id 데이터를 넣어주시면 됩니다."
+    )
 
     @PostMapping("/")
     public ApiResponse<DiaryCategoryResponseDTO.CreateCategoryResultDTO> create (@RequestBody @Valid DiaryCategoryRequestDTO.CreateCategoryDTO request) {
