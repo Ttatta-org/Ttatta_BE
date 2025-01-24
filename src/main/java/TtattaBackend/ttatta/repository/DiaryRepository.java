80ae09ce206a79309dec12c7cbc2b8d2a0dce8a1
BAEK0111 <baekjw111@gmail.com>
2025-01-24 19:30:22 +0900

    refactor: #48 develop 충돌 해결
    일기 조회 api 성공
    일기 삭제 api 수정 필요

--- src/main/java/TtattaBackend/ttatta/repository/DiaryRepository.java
+++ src/main/java/TtattaBackend/ttatta/repository/DiaryRepository.java
@@ -2,10 +2,18 @@ package TtattaBackend.ttatta.repository;
 
 import TtattaBackend.ttatta.domain.Diaries;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diaries, Long> {
     Integer countDiariesByDiaryCategoriesId(Long diaryCategoryId);
     Integer countDiariesByUsersId(Long userId);
+
+    @Modifying
+    @Query("UPDATE Diaries d SET d.diaryCategories.id = :targetCategoryId WHERE d.diaryCategories.id = :sourceCategoryId")
+    void updateCategoryForDiaries(@Param("sourceCategoryId") Long sourceCategoryId,
+                                  @Param("targetCategoryId") Long targetCategoryId);
 }
