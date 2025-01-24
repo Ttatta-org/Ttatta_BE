c71331125df018885e3a89de551dac797dd9dc3f
BAEK0111 <baekjw111@gmail.com>
2025-01-24 17:49:07 +0900

    fix: #48 일기 개수 조회 API
    주석 삭제

--- src/main/java/TtattaBackend/ttatta/web/dto/DiaryCategoryResponseDTO.java
+++ src/main/java/TtattaBackend/ttatta/web/dto/DiaryCategoryResponseDTO.java
@@ -75,12 +75,4 @@ public class DiaryCategoryResponseDTO {
     public static class DiaryCategoryExceptionDTO{
         Long categoryId;
     }
-
-//    @Builder
-//    @Getter
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    public static class DiaryCategoryColorExceptionDTO{
-//        String categoryColor;
-//    }
 }
