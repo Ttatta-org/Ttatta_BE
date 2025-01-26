7d2cc0f4c43c74ac0d07ad475b01806eb7a3d4d5
Kim YoonSu <121972417+yoonsu417@users.noreply.github.com>
2025-01-26 14:38:21 +0900

    refactor: #58 userId DTO -> token
    userId를 DTO가 아닌 token으로 처리 수정

--- src/main/java/TtattaBackend/ttatta/web/controller/DiaryController.java
+++ src/main/java/TtattaBackend/ttatta/web/controller/DiaryController.java
@@ -24,14 +24,14 @@ public class DiaryController {
 
     @Operation(summary = "일기 작성",
             description = """
-                    사용자의 id, 카테고리 id, 일기 내용, 일기 사진, 사진 찍은 날짜, 위도, 경도, 위치이름을 작성해주세요.
+                    카테고리 id, 일기 내용, 일기 사진, 사진 찍은 날짜, 위도, 경도, 위치이름을 작성해주세요.
                     저장된 일기의 id와 사진 찍은 날짜가 반환됩니다.
                     """
     )
 
     @PostMapping(value = "/post", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ApiResponse<DiaryResponseDTO.PostResultDTO> diarySave(@Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-                                                                          @RequestPart @Valid DiaryRequestDTO.PostDTO request,
+    public ApiResponse<DiaryResponseDTO.PostResultDTO> diarySave(   @Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+                                                                 @RequestPart @Valid DiaryRequestDTO.PostDTO request,
                                                                  @RequestPart("image") MultipartFile diaryPhotos){
         Diaries diaries = diaryCommandService.save(request, diaryPhotos);
 
@@ -42,13 +42,12 @@ public class DiaryController {
 
     @Operation(summary = "일기 삭제",
             description = """
-                    사용자의 id, 일기 id를 작성해주세요.
+                    일기 id를 작성해주세요.
                     해당 일기가 삭제됩니다.
                     """
     )
     @DeleteMapping("/delete/{diaryId}")
-    public ApiResponse<Object> deleteDiary(@PathVariable  Long diaryId,
-                                           @RequestBody @Valid DiaryRequestDTO.DeleteDTO request) {
+    public ApiResponse<Object> deleteDiary(@PathVariable  Long diaryId) {
         diaryCommandService.delete(diaryId);
 
         return ApiResponse.onSuccess("");
@@ -56,7 +55,7 @@ public class DiaryController {
 
     @Operation(summary = "일기 수정",
             description = """
-                    사용자 id, 일기 id, 수정할 일기의 내용을 작성해주세요.
+                    일기 id, 수정할 일기의 내용을 작성해주세요.
                     현재는 내용만 수정 가능합니다.
                     """
     )
@@ -73,7 +72,7 @@ public class DiaryController {
 
     @Operation(summary = "일기 보관함",
         description = """
-                사용자 id, 날짜, 페이징 번호를 작성해주세요.
+                날짜(선택), 페이징 번호(필수)를 작성해주세요.
                 일기 보관함 화면에서 조회할 수 있는 일기가 최신순으로 반환됩니다.
                 """
     )
@@ -85,7 +84,7 @@ public class DiaryController {
 
     @Operation(summary = "일기 지도",
         description = """
-                사용자 id, 위도, 경도, 페이징 번호를 작성해주세요.
+                위도, 경도, 페이징 번호를 작성해주세요.
                 일기 지도 화면에서 발자국 컴포넌트 클릭 시 그 위치의 일기가 반환됩니다.
                 """
     )
@@ -97,7 +96,7 @@ public class DiaryController {
 
     @Operation(summary = "일기 검색",
         description = """
-                사용자 id, 검색 내용, 페이징 번호를 작성해주세요.
+                검색 내용, 페이징 번호를 작성해주세요.
                 검색한 내용이 들어가 있는 일기가 반환됩니다.
                 """
     )
