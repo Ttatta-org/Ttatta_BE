df247686f8db26640c31b16996e6238b75de46ec
Kim YoonSu <121972417+yoonsu417@users.noreply.github.com>
2025-02-19 21:01:16 +0900

    feat: #131 일기 사진 리사이징
    일기 사이즈를 일정 크기 이하로 지정하여 저장

--- src/main/java/TtattaBackend/ttatta/aws/s3/AmazonS3Manager.java
+++ src/main/java/TtattaBackend/ttatta/aws/s3/AmazonS3Manager.java
@@ -4,15 +4,24 @@ import TtattaBackend.ttatta.config.AmazonConfig;
 import TtattaBackend.ttatta.domain.Uuid;
 import TtattaBackend.ttatta.repository.UuidRepository;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import marvin.image.MarvinImage;
+import org.marvinproject.image.transform.scale.Scale;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Component
@@ -26,14 +35,19 @@ public class AmazonS3Manager{
     private final UuidRepository uuidRepository;
 
     public String uploadFile(String keyName, MultipartFile file){
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
+        String originalFilename = file.getOriginalFilename();
+        String fileFormatName = file.getContentType().substring(file.getContentType().lastIndexOf("/") + 1);
 
+        MultipartFile resizedFile = resizeImageByMarvin(keyName, originalFilename, fileFormatName, file, 400);
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(resizedFile.getSize());
         metadata.setContentDisposition("inline");
         metadata.setContentType(file.getContentType());
 
-        try {
-            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName, file.getInputStream(), metadata));
+        try(InputStream inputStream = resizedFile.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName, inputStream, metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (IOException e){
             log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
         }
@@ -54,6 +68,36 @@ public class AmazonS3Manager{
             amazonS3.deleteObject(new DeleteObjectRequest(amazonConfig.getBucket(), keyName));
         } catch (Exception e) {
             log.error("error at AmazonS3Manager deleteFile : {}", (Object) e.getStackTrace());
+        }
+    }
+
+    MultipartFile resizeImageByMarvin(String fileName, String originalFilename, String fileFormatName, MultipartFile originalImage, int targetWidth) {
+        try {
+            // MultipartFile -> BufferedImage Convert
+            BufferedImage image = ImageIO.read(originalImage.getInputStream());
+            // newWidth : newHeight = originWidth : originHeight
+            int originWidth = image.getWidth();
+            int originHeight = image.getHeight();
+
+            // origin 이미지가 resizing될 사이즈보다 작을 경우 resizing 작업 안 함
+            if(originWidth < targetWidth)
+                return originalImage;
+            MarvinImage imageMarvin = new MarvinImage(image);
+
+            Scale scale = new Scale();
+            scale.load();
+            scale.setAttribute("newWidth", targetWidth);
+            scale.setAttribute("newHeight", targetWidth * originHeight / originWidth);
+            scale.process(imageMarvin.clone(), imageMarvin, null, null, false);
+
+            BufferedImage imageNoAlpha = imageMarvin.getBufferedImageNoAlpha();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(imageNoAlpha, fileFormatName, baos);
+            baos.flush();
+
+            return new CustomMultipartFile(fileName, originalFilename, fileFormatName, baos.toByteArray());
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이즈에 실패했습니다.");
         }
     }
 }
