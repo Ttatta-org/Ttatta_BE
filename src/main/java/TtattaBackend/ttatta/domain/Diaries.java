1391b27b1d3c2cfe2afb51e0c558021b90a713d7
BAEK0111 <baekjw111@gmail.com>
2025-08-31 11:14:03 +0900

    feat: #198 부분암호화 - 필요 엔티티 추가

--- src/main/java/TtattaBackend/ttatta/domain/Diaries.java
+++ src/main/java/TtattaBackend/ttatta/domain/Diaries.java
@@ -57,6 +57,37 @@ public class Diaries extends BaseEntity {
 
     private Long clusterId;
 
+    // ====== [추가] 암호화 좌표 관련 필드 ======
+
+    // ~Cipher : 평문 위도, 경도를 AES-GCM으로 암호화한 결과값
+    // ciphertext + GCM 인증 태그
+    @Lob @Column(name = "lat_ct", nullable = false)
+    private byte[] latCipher;
+    @Lob @Column(name = "lng_ct", nullable = false)
+    private byte[] lngCipher;
+
+    // AES-GCM에 사용하는 초기화 벡터(IV)
+    // GCM은 동일한 키 또는 IV로 암호화하면 치명적인 보안 문제가 발생하기 때문에 랜덤으로 생성
+    @Column(name = "iv_lat", nullable = false)
+    private byte[] ivLat;
+    @Column(name = "iv_lng", nullable = false)
+    private byte[] ivLng;
+
+    // 실제 AES-GCM 암호화에 사용된 데이터 암호화 키(DEK)를 AWS KMS로 래핑한 결과
+    // DB에는 평문 DEK가 절대 저장되지 않고, 암호화된 DEK만 저장됨.
+    // 복호화 시에는 KMS에 dekWrapped를 보내서 평문 DEK를 다시 얻는다.
+    @Lob @Column(name = "dek_wrapped", nullable = false)
+    private byte[] dekWrapped;
+
+    // 어떤 KMS를 사용해 DEK를 래핑했는지 식별하기 위한 ARN
+    @Column(name = "kms_key_id", nullable = false, length = 200)
+    private String kmsKeyId;
+
+    // 암호화 버전
+    @Column(name = "enc_ver", nullable = false)
+    private short encVer = 1;
+
+
     public void setUsers(Users users) {
         // 기존에 이미 등록되어 있던 관계를 제거
         if (this.users != null) {
